6db8dd7f2e40f0e893c1f07a189fafcbddca23b5
leivaa <adrianleiva212@gmail.com>
2022-11-15 17:54:35 +0100

    feat: added storybook to the project

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.tsx"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  framework: "@storybook/react",
+  core: {
+    "builder": "@storybook/builder-webpack5"
+  },
+  staticDirs: ['../public'],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: false,
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+}
