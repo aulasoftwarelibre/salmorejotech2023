256d61612c250af6cdb7cc37069e18a01bd05276
leivaa <adrianleiva212@gmail.com>
2022-11-16 17:35:59 +0100

    feat: parametized button story, now its editable the label of the button

--- src/stories/Components/button.stories.tsx
+++ src/stories/Components/button.stories.tsx
@@ -5,6 +5,17 @@ import Button from "../../components/button";
 export default {
   title: 'Components/Buttons',
   component: Button,
+  argTypes: {
+    label: {
+      name: 'label',
+      type: { name: 'string', required: true },
+      defaultValue: 'Example',
+      description: 'Label displayed on button',
+      control: {
+        type: 'text',
+      }
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
-export const Entradas: ComponentStory<typeof Button> = () => <Button label="ENTRADAS" />;
+export const Customizable: ComponentStory<typeof Button> = (args) => <Button {...args} />;
