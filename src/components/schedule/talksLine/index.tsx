ba105a9c4b5aefc7023dffd902919768b44db67a
JJtrenado <juanjotrenadozafra@protonmail.com>
2022-11-19 19:27:25 +0100

    feat: :construction:

--- src/components/schedule/talksLine/index.tsx
+++ src/components/schedule/talksLine/index.tsx
@@ -1,16 +1,35 @@
 import styles from "./talksLine.module.css";
+import Image from 'next/image';
 
-export const TalksLine = (props: { id: string, title: string, info: string, date: string, labels: Array<string>, isBreak: boolean, track: number, imageUrl: string, name: string}) => {
+
+export const TalksLine = (props: { id: string, title: string, info: string, date: string, labels: Array<string>, track: number, imageUrl: string, name: string}) => {
   let hours:string[]=props.date.split(" ");
 
   return (
     <div className={styles.container}>
+
       <div className={styles.date}>
         {hours.map((hour,index)=>{
           return <h4 key={index}>{hour}</h4>
         })}
       </div>
-      <h3 className={styles.title}>{props.title}</h3>
+      
+      <div className={styles.talkInfo}>
+        <div>
+          <h3>{props.title}</h3>
+          <h4>{props.name}</h4>
+          <div className={styles.labels}>
+            {props.labels.map((hour,index)=>{
+              return <div key={index}>{hour}</div>
+            })}
+          </div>
+        </div>
+        
+        <div>
+        <Image src="https://pbs.twimg.com/profile_images/1352946531628969984/NNfdBgII_400x400.jpg" width={150} height={150} alt={"Speaker image"} />
+        </div>
+      </div>
+
     </div>
   );
 };
--- src/stories/Components/talksLine.stories.tsx
+++ src/stories/Components/talksLine.stories.tsx
@@ -29,27 +29,12 @@ export default {
 } as ComponentMeta<typeof TalksLine>;
 
 
-let Talk1: Talk = {
-  id: '6e5436',
-  title: 'charla1',
-  info: 'no se que se pone aqui',
-  date: '00:00 00:00',
-  labels: ['front', 'ux'],
-  isBreak: false,
-  track: 0,
-  //Speaker info
-  imageUrl: 'https://pbs.twimg.com/profile_images/1352946531628969984/NNfdBgII_400x400.jpg',
-  name: "Juanjo"
-}
-
-
 export const TalksLine_Primary: ComponentStory<typeof TalksLine> = (args) => (
   <TalksLine id= {'6e5436'}
     title= {'charla1'}
     info= {'no se que se pone aqui'}
     date= {'00:00 00:00'}
     labels= {['front', 'ux']}
-    isBreak= {false}
     track= {0}
     //Speaker info
     imageUrl= {'https://pbs.twimg.com/profile_images/1352946531628969984/NNfdBgII_400x400.jpg'}
