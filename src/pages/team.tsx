69303aaea2a0f1cfe200fb49830a81b658027cea
leivaa <adrianleiva212@gmail.com>
2023-01-27 15:44:17 +0100

    fix: updated imports on pages where navbar was imported with {} instead of default import

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import { Footer } from '../components/footer'
 import GridPonentes from '../components/gridPonentes'
 import { Moleculas } from '../components/moleculas'
-import { Navbar } from '../components/navbar'
+import Navbar from '../components/navbar'
 import { useContentWritter } from '../hooks/useContentWritter'
 import styles from '../styles/team.module.css'
 
