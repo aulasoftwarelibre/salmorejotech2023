382663041fcdea77fb478c2c0ee2b912ce8f246a
leivaa <adrianleiva212@gmail.com>
2023-01-27 23:20:15 +0100

    feat: now printing actual contact link instead of hard-coded ones

--- src/components/speakers/index.tsx
+++ src/components/speakers/index.tsx
@@ -1,16 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import styles from './speakers.module.css'
-import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { BsTwitter, BsGithub, BsLinkedin, BsGlobe } from 'react-icons/bs';
+
+export interface Contact {
+  type: 'TWITTER'|'WEB'|'LINKEDIN'|'GITHUB',
+  link: string,
+}
 
 export interface SpeakerProps {
   name: string;
   bio: string;
   urlPhoto: string;
-  contacts: Array<{ type: string, link: string }>;
+  contacts: Array<Contact>;
+}
+
+const LinkTypeAsIcon = {
+  'TWITTER': <BsTwitter/>,
+  'WEB': <BsGlobe/>,
+  'LINKEDIN': <BsLinkedin/>,
+  'GITHUB': <BsGithub/>,
 }
 
 export const SpeakerCard = ({name, bio, urlPhoto, contacts}:  SpeakerProps) => {
+
+  const renderContactLink = (contact: Contact) => {
+    if(!LinkTypeAsIcon[contact.type]) return;
+    return (
+      <a href={contact.link} aria-label={`${name}'s link for ${contact.type}`}>{LinkTypeAsIcon[contact.type]}</a>
+    )
+  }
+
   return (
     <div className={styles.card}>
       <img className={styles.speakerImage} src={urlPhoto} alt="Imagen del ponente" />
@@ -19,9 +39,7 @@ export const SpeakerCard = ({name, bio, urlPhoto, contacts}:  SpeakerProps) => {
         <p className={styles.subtitle}>{bio}</p>
       </div>
       <div className={styles.socialMedia}>
-        <a href="https://twitter.com/AulaSL?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><BsTwitter /></a>
-        <a href="https://github.com/aulasoftwarelibre"><BsGithub /></a>
-        <a href="https://www.linkedin.com/company/aulasoftwarelibre/mycompany/"><BsLinkedin /></a>
+        {contacts.map(contact => renderContactLink(contact))}
       </div>
     </div>
   )
