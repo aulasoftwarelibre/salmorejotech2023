a8dc4b9e894cc7af3fdade3d4d77dd47a80c2f61
JJtrenado <juanjotrenadozafra@protonmail.com>
2023-03-16 17:34:46 +0100

    style: :art: label colors changed

--- src/contexts/talk/domain/TalkLabelMap.ts
+++ src/contexts/talk/domain/TalkLabelMap.ts
@@ -1,9 +1,9 @@
 import { LabelAsPrimitives } from "./TalkLabel";
 
 const enum LabelColor {
-  Red = "#DD3F4E",
+  Purple = "#cc00ff",
   Yellow = "#FAD000",
-  Lime = "#7ECC49",
+  Lime = "#1fb800",
   Mint = "#6ACCBC",
   Blue = "#14AAF5",
   Lavender = "#EB96EB"
@@ -12,8 +12,8 @@ const enum LabelColor {
 
 export const TalkLabelMap = new Map<string, LabelAsPrimitives>([
   ['QA', {text: 'QA', color: LabelColor.Lime}],
-  ['Backend', {text: 'Backend', color: LabelColor.Blue}],
-  ['Frontend', {text: 'Frontend', color: LabelColor.Red}],
+  ['Backend', {text: 'Backend', color: LabelColor.Purple}],
+  ['Frontend', {text: 'Frontend', color: LabelColor.Blue}],
   ['PM', {text: 'Project Management', color: LabelColor.Mint}],
   ['Infra', {text: 'Infrastructure', color: LabelColor.Yellow}]
 ])
