39abc9537e1568efa181f07c119439a408a5a94c
JJtrenado <juanjotrenadozafra@protonmail.com>
2023-03-30 15:12:41 +0200

    style: :art: break line aula colors

--- src/components/schedule/components/Break.tsx
+++ src/components/schedule/components/Break.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/index.module.css";
 
 export const Break = ({title}: {title: string}) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.breakContainer}>
       <h3 className={styles.breakTitle}>{title}</h3>
     </div>
   );
