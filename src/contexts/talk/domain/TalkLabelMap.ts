13b5a951817cf20550de1623e686359799499a6e
Leivaa <56723478+leivaa21@users.noreply.github.com>
2023-04-25 11:37:22 +0200

    feat: schedule (#31)
    * wip: workin on schedule
    
    * feat: finish schedule data
    
    * fix: added space between title and info of talks
    
    * feat: updated data
    
    * style: improved css for schedule
    
    * feat: new labels

--- src/contexts/talk/domain/TalkLabelMap.ts
+++ src/contexts/talk/domain/TalkLabelMap.ts
@@ -2,10 +2,11 @@ import { LabelAsPrimitives } from "./TalkLabel";
 
 const enum LabelColor {
   Purple = "#b51963",
-  Yellow = "#2a5547",
+  Yellow = "#FFEA00",
   Lime = "#00bf7d",
   Mint = "#00b4c5",
   Blue = "#0073e6",
+  LightBlue = '#6495ED',
   Pink = "#f194b8",
   Black = "#000000",
   White = "#ffffff"
@@ -17,5 +18,7 @@ export const TalkLabelMap = new Map<string, LabelAsPrimitives>([
   ['Frontend', {text: 'Frontend', textColor: LabelColor.White, color: LabelColor.Blue}],
   ['Cyber', {text: 'Ciberseguridad', textColor: LabelColor.Black, color: LabelColor.Mint}],
   ['IA', {text: 'IA', textColor: LabelColor.Black, color: LabelColor.Pink}],
-  ['General', {text: 'General', textColor: LabelColor.Black, color: LabelColor.White}],
+  ['Career', {text: 'Dev Career', textColor: LabelColor.Black, color: LabelColor.White}],
+  ['Data', {text: 'Data', textColor: LabelColor.Black, color: LabelColor.Yellow}],
+  ['DevOps', {text: 'DevOps', textColor: LabelColor.Black, color: LabelColor.LightBlue}],
 ])
